2bba6edf4ec103496c955e3ced582085f962c8cb
SchaafLaura <schaaf.laura92@gmail.com>
2025-04-07 20:04:34 +0200

    silly cleanup

--- ECStest/Components/IComponent.cs
+++ ECStest/Components/IComponent.cs
@@ -1,5 +1,29 @@
+using System.Collections.Frozen;
+using System.Reflection;
+
 namespace ECSTest.Components;
 public interface IComponent
 {
     public void Update(int id);
+}
+
+internal static class ComponentExtensions
+{
+    public static readonly FrozenDictionary<Type, int> TypesToID;
+    public static int NumTypes => TypesToID.Count;
+    static ComponentExtensions()
+    {
+        var componentType = typeof(IComponent);
+        var subclassTypes = Assembly
+            .GetAssembly(typeof(IComponent))!
+            .GetTypes()
+            .Where(t => componentType.IsAssignableFrom(t) && t.IsClass);
+        var dict = new Dictionary<Type, int>();
+        var k = 0;
+        foreach (var t in subclassTypes)
+            dict.Add(t, k++);
+        TypesToID = dict.ToFrozenDictionary();
+    }
+
+    public static int GetID(this IComponent c) => TypesToID[c.GetType()];
 }
--- ECStest/ECS.cs
+++ ECStest/ECS.cs
@@ -1,6 +1,4 @@
-using System.Collections.Frozen;
 using ECSTest.Components;
-using System.Reflection;
 
 namespace ECSTest;
 public class ECS
@@ -8,23 +6,12 @@ public class ECS
     private static int runningID;
     private readonly int _maxEntities;
     private readonly IComponent[][] _systems;
-    private readonly FrozenDictionary<Type, int> _typesToID;
+    
     public ECS(int maxEntities)
     {
         _maxEntities = maxEntities;
-        var componentType = typeof(IComponent);
-        var subclassTypes = Assembly
-            .GetAssembly(typeof(IComponent))!
-            .GetTypes()
-            .Where(t => componentType.IsAssignableFrom(t) && t.IsClass);
-        var dict = new Dictionary<Type, int>();
-        var k = 0;
-        foreach (var t in subclassTypes)
-            dict.Add(t, k++);
-        _typesToID = dict.ToFrozenDictionary();
-        
-        _systems = new IComponent[_typesToID.Count][];
-        foreach (var (type, id) in _typesToID)
+        _systems = new IComponent[ComponentExtensions.NumTypes][];
+        foreach (var (type, id) in ComponentExtensions.TypesToID)
             _systems[id] = (IComponent[]) Array.CreateInstance(type, maxEntities);
     }
     public void Update()
@@ -36,7 +23,7 @@ public class ECS
     public Entity AddEntity(params IComponent[] components)
     {
         foreach (var c in components)
-            _systems[_typesToID[c.GetType()]][runningID] = c;
+            _systems[c.GetID()][runningID] = c;
         return new Entity(runningID++);
     }
 }
